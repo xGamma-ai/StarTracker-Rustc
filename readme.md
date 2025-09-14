6618cc0bd3e2101143589851b2b3bbfc1f75a61a
alwin t varghese <alwintv5018@gmail.com>
2025-09-14 11:34:20 +0530

    Update project configuration and enhance README
    - Set Rust version to 1.89.0 in Cargo.toml.
    - Defined binary target for the project in Cargo.toml.
    - Updated README with instructions for building the server and changed local development binding to 127.0.0.1.
    - Renamed manual_hello function to health_check and updated its response message.
    - Changed server binding from 0.0.0.0 to 127.0.0.1 for local development.

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # StarTracker Rustc
 
 The standalone API server for StarTracker Native app.
+
+## To actively build the server ```src``` file
+```shell
+cargo install cargo-watch
+cargo watch -w src -x run
+```
+
+## Change binding from 0.0.0.0 to 127.0.0.1 for local dev.
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("The Server is alive.")
 }
 
 #[actix_web::main]
@@ -20,9 +20,9 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/health", web::get().to(health_check))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
