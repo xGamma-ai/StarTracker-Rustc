829974dd814e040fde228657f6015a8e41b8fb22
alwin t varghese <alwintv5018@gmail.com>
2025-10-02 17:20:05 +0530

    Refactor `verify_user` endpoint to improve error handling and response format
    - Updated the `verify_user` function to use `expect` for extracting user email from JWT claims, simplifying error handling.
    - Changed the response to include the user's email instead of the user name in the success message.

--- src/user_access_management/get.rs
+++ src/user_access_management/get.rs
@@ -7,16 +7,18 @@ use crate::{
 
 #[get("/verify-user")]
 async fn verify_user<'a>(req: HttpRequest) -> impl Responder {
-    let user_struct = match req.extensions().get::<Claims>() {
-        Some(c) => c.user_details.user_email.clone(),
-        None => return HttpResponse::Unauthorized().body("User details not found in token."),
-    };
-    println!("incoming email {user_struct}");
+    let user_struct = req
+        .extensions()
+        .get::<Claims>()
+        .expect("Failed to extract claims data")
+        .user_details
+        .user_email
+        .to_string();
     use self::user_data::dsl::*;
     let results = user_data
         .filter(user_email.eq(&user_struct))
         .select(UserData::as_select())
         .first(&mut establish_connection())
         .expect("Error pulling user data.");
-    HttpResponse::Ok().body(format!("Hello world! {}", results.user_name))
+    HttpResponse::Ok().body(format!("Hello world! {}", results.user_email))
 }
